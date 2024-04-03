6db1e27de55e700a9e0e0880e308eb77c225ea3a
Ahmed Saqr <ahmedhassansaqr28@gmail.com>
2024-04-03 13:06:59 +0200

    Add Failure class and update HomeRepo to return Either monad

--- lib/features/home/domain/repos/home_repo.dart
+++ lib/features/home/domain/repos/home_repo.dart
@@ -1,6 +1,8 @@
+import 'package:bookly_app/core/errors/failrue.dart';
+import 'package:dartz/dartz.dart';
 import '../entities/book_entity.dart';
 
 abstract class HomeRepo {
-  Future<List<BookEntity>> fetchFeaturedBooks();
-  Future<List<BookEntity>> fetchNewestBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks();
 }
