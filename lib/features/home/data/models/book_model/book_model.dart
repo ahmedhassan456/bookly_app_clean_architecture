e9a92015318f8b688f823a798a9661427e732384
Ahmed Saqr <ahmedhassansaqr28@gmail.com>
2024-04-12 20:19:12 +0200

    create api service

--- lib/features/home/data/models/book_model/book_model.dart
+++ lib/features/home/data/models/book_model/book_model.dart
@@ -1,9 +1,11 @@
+import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
+
 import 'access_info.dart';
 import 'sale_info.dart';
 import 'search_info.dart';
 import 'volume_info.dart';
 
-class BookModel {
+class BookModel extends BookEntity{
   String? kind;
   String? id;
   String? etag;
@@ -22,7 +24,13 @@ class BookModel {
     this.saleInfo,
     this.accessInfo,
     this.searchInfo,
-  });
+  }) : super(bookId: id!,
+   image: volumeInfo?.imageLinks?.thumbnail ?? '', 
+   title: volumeInfo!.title!,
+   authorName: volumeInfo.authors?.first ?? '', 
+   price: saleInfo?.retailPrice?.amount ?? 0.0, 
+   rating: 4.8,
+   );
 
 
   factory BookModel.fromJson(Map<String, dynamic> json) => BookModel(
