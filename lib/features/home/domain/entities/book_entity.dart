6ded28793a625ce7c15a6e71135ad7faa6c1c5b6
Ahmed Saqr <ahmedhassansaqr28@gmail.com>
2024-04-13 12:43:31 +0200

    generate book entity adapter

--- lib/features/home/domain/entities/book_entity.dart
+++ lib/features/home/domain/entities/book_entity.dart
@@ -1,9 +1,20 @@
+import 'package:hive/hive.dart';
+
+part 'book_entity.g.dart';
+
+@HiveType(typeId: 0)
 class BookEntity {
+  @HiveType(typeId: 0)
   final String bookId;
+  @HiveType(typeId: 1)
   final String? image;
+  @HiveType(typeId: 2)
   final String title;
+  @HiveType(typeId: 3)
   final String? authorName;
+  @HiveType(typeId: 4)
   final num? price;
+  @HiveType(typeId: 5)
   final num? rating;
 
   BookEntity(
---
+++ lib/features/home/domain/entities/book_entity.g.dart
@@ -0,0 +1,36 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'book_entity.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class BookEntityAdapter extends TypeAdapter<BookEntity> {
+  @override
+  final int typeId = 0;
+
+  @override
+  BookEntity read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return BookEntity();
+  }
+
+  @override
+  void write(BinaryWriter writer, BookEntity obj) {
+    writer.writeByte(0);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is BookEntityAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
