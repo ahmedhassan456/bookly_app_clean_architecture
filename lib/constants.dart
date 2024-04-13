cd6f2074010b0c6e502238549627dc42f3f6299b
Ahmed Saqr <ahmedhassansaqr28@gmail.com>
2024-04-13 12:48:50 +0200

    register adapter and open featured box

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,3 +3,4 @@ import 'dart:ui';
 const Color primaryColor = Color(0xFF100B20);
 const Duration transitionDuration = Duration(milliseconds: 250);
 const String kGtSectraFine = 'GT Sectra Fine';
+const String kFeaturedBox = 'featured_box';
